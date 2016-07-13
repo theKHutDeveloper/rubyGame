230d90893bb41a030c5c64a4617a76c963bfb04c
KHutDeveloper <angie@andriens-mini.home>
2016-07-13 22:20:16 +0100

    added a new background image and added velocity to player.rb

--- game_window.rb
+++ game_window.rb
@@ -1,28 +1,23 @@
 require 'gosu'
-require 'gosu_tiled'
-require_relative 'player.rb'
 require_relative 'background.rb'
+require_relative 'player.rb'
+
 
 class GameWindow < Gosu::Window
   def initialize
-    @WIDTH = 1000
-    @HEIGHT = 600
+    @WIDTH = 640
+    @HEIGHT = 480
 
-    super 640, 480
+    super @WIDTH, @HEIGHT
     self.caption = "Game Demo Window"
     #@player = Person.new
+    @backgnd = Background.new(self)
     @player = Player.new(self)
-    @map = Background.new(self)
-    @player.set_position(100,200)
-    @camera_x = @camera_y = 300
+    @player.set_position(100,280)
   end
 
   def update
-    @map.update
     @player.update
-    # Scrolling follows player
-    @camera_x = [[@player.get_x - @WIDTH / 2, 0].max, @map.width * 50 - @WIDTH].min
-    @camera_y = [[@player.get_y - @HEIGHT / 2, 0].max, @map.height * 50 - @HEIGHT].min
   end
 
   def button_down id
@@ -30,10 +25,8 @@ class GameWindow < Gosu::Window
   end
 
   def draw
-    Gosu::translate(-@camera_x, -@camera_y) do
-      @map.draw
-      @player.draw
-    end
+    @backgnd.draw
+    @player.draw
   end
 end
 
--- player.rb
+++ player.rb
@@ -1,34 +1,37 @@
-#require 'gosu'
-
 class Player
+
+  GRAVITY = 100
+  X_GRAVITY = 20
+  JUMP_VELOCITY = 100
 
   def initialize window
     @window = window
     @score = 0
-    @x = @y = @velocity_x = @velocity_y = @gravity_x = @gravity_y = 0
+    @player_x = @player_y = @velocity_x = @velocity_y = @gravity_x = @gravity_y = 0
     @frame = 0
     @direction = :right
     @moving = false
     @counter = 10
     @jumping = false
+    @angle = 0;
 
-    @idle = Gosu::Image.load_tiles @window, "assets/monkey_idle.png", 57, 100, true
     @walk = Gosu::Image.load_tiles @window, "assets/monkey_walk.png", 57, 100, true
     @jump = Gosu::Image.load_tiles @window, "assets/monkey_jump.png", 57, 100, true
-
+    @idle = Gosu::Image.load_tiles @window, "assets/monkey_idle.png", 57, 100, true
   end
 
   def set_position(x,y)
-    @x, @y = x, y
+    @player_x, @player_y = x, y
   end
 
   def get_x
-    return @x
+    @player_x
   end
 
   def get_y
-    return @y
+    @player_y
   end
+
 
   def update
     @moving = false
@@ -42,26 +45,40 @@ class Player
       @frame += 1
     end
 
+    #handle jumping
+    if @velocity_y != 0
+      delta_time = (@window.update_interval / 1000)
+      @velocity_y += GRAVITY * delta_time
+      @player_y += @velocity_y * delta_time
+      if @player_y > 280
+        @velocity_y = 0
+        @player_y = 280
+      end
+    end
+
     if @window.button_down? Gosu::KbLeft
       @direction = :left
       @moving = true
-      @x += -5
+      delta_time = (@window.update_interval / 1000)
+      @velocity_x += X_GRAVITY * delta_time
+      @player_x -= @velocity_x * delta_time
+
     elsif @window.button_down? Gosu::KbRight
       @direction = :right
       @moving = true
-      @x += 5
+      delta_time = (@window.update_interval / 1000)
+      @velocity_x += X_GRAVITY * delta_time
+      @player_x += @velocity_x * delta_time
     end
 
-
-     if @window.button_down? Gosu::KbUp
-       @y += -5
+    if @window.button_down? Gosu::KbUp
+       @velocity_y = -JUMP_VELOCITY
        @jumping = true
-     end
+    end
 
   end
 
   def draw
-    #count = 0
 
     if @moving
       count = @frame %@walk.size
@@ -77,9 +94,9 @@ class Player
     end
 
     if @direction == :right
-      image.draw @x, @y, 1
+      image.draw @player_x, @player_y, 1 #0.9, 0.9
     else
-      image.draw @x + image.width, @y, 1, -1
+      image.draw @player_x + image.width, @player_y, 1, -1 #-0.9, 0.9
     end
 
   end
